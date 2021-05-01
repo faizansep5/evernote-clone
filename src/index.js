4bc066d73c2156a67adad1163b770f0e617ea6e2
Faizan <faizansep5@gmail.com>
2021-05-01 19:33:18 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const firebase = require("firebase").default;
+require("firebase/firestore");
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1F4O_BTSYC0WF_SH-2XPw0MgrpMBtN2U",
+  authDomain: "evernote-clone-ec9ec.firebaseapp.com",
+  projectId: "evernote-clone-ec9ec",
+  storageBucket: "evernote-clone-ec9ec.appspot.com",
+  messagingSenderId: "143621907443",
+  appId: "1:143621907443:web:b6c249bdfbd7bef60d6886",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
