6de25043894b620f8f9b2fac89fc09bd126d04b1
Faizan <faizansep5@gmail.com>
2021-05-01 20:17:56 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
     const newID = newFromDB.id;
     await this.setState({ notes: [...this.state.notes, note] });
     const newNoteIndex = this.state.notes.indexOf(
-      this.state.notes.filter((_note) => _note.id === newID[0])
+      this.state.notes.filter((_note) => _note.id === newID)[0]
     );
     this.setState({
       selectedNote: this.state.notes[newNoteIndex],
